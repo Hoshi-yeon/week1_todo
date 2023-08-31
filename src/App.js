c98b3926045488df589ab22b5760096a5d615519
Kim chaeyeon <gk13578@gmail.com>
2023-08-31 10:52:01 +0900

    todolist_2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([
+      {id: 1, desc: "react 어려ㅜ어요..."},
+      {id: 2, desc: "ㅜㅜㅜㅜㅜ너무"}
+  ]);
+
+  const [desc, setDesc] = useState('');
+
+  const nameChangeHandler = (event) => {
+    setDesc(event.target.value)
+  };
+
+  // 추가 버튼
+  const clickAddButtonHandler = (event) => {
+    const newUser = {
+      id: users.length + 1,
+      desc
+    }
+    setUsers([...users, newUser]);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='mainBox'>
+      <div>
+        <input value={desc} onChange={nameChangeHandler}/>
+        <button onClick={clickAddButtonHandler}>추가하기</button>
+        <h2>Todo List</h2>
+      </div>
+      <div className='app-style'>
+        {users.map(function (item) {
+          return (
+            <div key={item.id} className='component-style'>
+              {item.desc}
+            </div>
+          );
+        })
+        }
+      </div>
     </div>
   );
-}
+};
+
+
 
 export default App;
